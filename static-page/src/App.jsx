49f03a9aed2afe6512fbbd858e7e9e2781febd61
yasin-wajihaa <wajihaazafar@gmail.com>
2026-02-20 20:28:50 +0530

    add static page project

---
+++ static-page/src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/navbar"
+import Mainnav from "./components/mainnav"
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Mainnav />
+    </>
+  )
+}
---
+++ static-page/src/components/mainnav.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className = "facts-list">
+                <li>Was first release in 2013</li>
+                <li>Was originally created by Jordan Walker</li>
+                <li>Was well over 200k stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
