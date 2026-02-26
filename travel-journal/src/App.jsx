debbba9d59702611db7113cf909e070d958b5b29
yasin-wajihaa <wajihaazafar@gmail.com>
2026-02-27 00:39:40 +0530

    Implement dynamic rendering of journal entries using map() and structured props

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./components/header.jsx"
+import Entry from "./components/entry.jsx"
+import data from "./components/data.js"
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+
+    const entryElements = data.map((entry) => {
+        return (
+            <Entry
+                key={entry.id}
+                entry={entry}
+            />
+        )
+    })
+
+    return (
+      <>
+          <Header />
+          <main className="container">
+            {entryElements}
+          </main>
+          
+      </>
+    )
 }
-
-export default App
---
+++ travel-journal/src/components/entry.jsx
@@ -0,0 +1,33 @@
+export default function Entry(props) {
+    return (
+        <article className="journal-entry">
+            <div className="mondstadt-image-container">
+                <img 
+                    className="mondstadt-image" 
+                    src={props.entry.img.src}
+                    alt={props.entry.img.alt}
+                />
+            </div>
+
+            <div className= "entry-content">
+                <div className = "entry-location">
+                    <img 
+                        className= "marker" 
+                        src="location-pin.png" 
+                        alt="marker icon" 
+                    />
+                    <span className= "country"> {props.entry.country} </span>
+                    <a 
+                        href={props.entry.googleMapsLink}
+                        target="_blank"> View on Google Maps </a>
+                </div>
+                
+                
+                <h2 className= "entry-title" > {props.entry.title} </h2>  
+                <p className= "entry-date" > {props.entry.dates}</p>   
+                <p className= "entry-text" > {props.entry.text}</p>      
+            </div>
+
+        </article>
+    )
+}
